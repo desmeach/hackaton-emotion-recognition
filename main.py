ceca26f1664d99fd751a95065f793df9723f3139
sergey shapovalov <shapovalov@intervolga.ru>
2022-10-28 19:44:14 +0300

    Hz che

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import tensorflow as tf
 import pandas as pd
 
 dataset = pd.read_excel("dataset/dataset_train.xlsx", usecols=[4])
-print(dataset)
+content = dataset.items()
+for row in content:
+    print(row)
