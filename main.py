1e60fb22a2ec348389f67cac8ba301f6fb425137
Ivan <clicker.heroes.acg@gmail.com>
2022-10-29 18:07:16 +0300

    upd

--- main.py
+++ main.py
@@ -1,51 +1,36 @@
 import pandas as pd
-# import keras as k
+import keras as k
 import numpy as np
-from presentation import Presentation
-from patient import Patient
-
-# from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation
 
 df = pd.read_excel("dataset/dataset_train.xlsx")
 tmp = df.groupby(['Obfuscated name', 'Presentation']).agg({'Data': list, 'Class_label_FPG': list})
 accum = tmp[['Data', 'Class_label_FPG']]
-input = []
-output = []
+inputs = []
+outputs = []
 
 for i, items in accum["Data"].items():
     tmp = []
     for item in items:
         arrayFromStr = item.replace('[', '').replace(']', '').replace(' ', '').split(',')
         arrayFromStr = [float(el) for el in arrayFromStr]
-        tmp.append(arrayFromStr)
-    input.append(tmp)
+        tmp.append(np.array(arrayFromStr))
+    inputs.append(np.array(tmp))
 
 for i, items in accum["Class_label_FPG"].items():
     temp = [float(elem) for elem in items]
-    output.append(temp)
+    outputs.append(np.array(temp))
 
-# names = dataset['Obfuscated name'].items()
-# datas = dataset['Data'].items()
-# presentations = dataset['Presentation'].items()
-# names_and_pres = dict()
-# for i, name in names:
-#     for j, presentation in presentations:
-#         d_list = []
-#         for k, data_list in datas:
-#             d_list.append()
-#         names_and_pres[name] = {
-#             presentation: data_list
-#         }
-#
-# print(names_and_pres)
-# model = k.Sequential(
-#     ([
-#         Dense(40, input_dim=inputs.shape[1]),
-#         Activation('relu'),
-#         Dense(20),
-#         Activation('softmax')
-#     ])
-# )
-# model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-# fit_results = model.fit(x=inputs, y=outputs, epochs=10)
-# print(fit_results)
+inputs = np.array(inputs)
+outputs = np.array(outputs)
+
+model = k.Sequential(
+    ([
+        Dense(40),
+        Activation('relu'),
+        Dense(20),
+        Activation('softmax')
+    ])
+)
+model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+fit_results = model.fit(x=inputs, y=outputs, epochs=10)
