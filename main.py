1fd974db8fb6161155ec4610d05702949ff55ffd
Ivan <clicker.heroes.acg@gmail.com>
2022-10-29 13:27:42 +0300

    working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pandas as pd
 import keras as k
 import numpy as np
+from keras.layers import Dense, Activation
 
 dataset = pd.read_excel("dataset/dataset_train.xlsx", skiprows=[0], usecols = [4, 5])
 content = dataset.iterrows()
@@ -14,7 +15,13 @@ for i, row in content:
     outputs.append(row.iloc[1])
 inputs = np.array(inputs)
 outputs = np.array(outputs)
-model = k.Sequential()
-model.add(k.layers.Dense(units=1, activation="linear"))
-model.compile(loss="mse", optimizer="sgd")
-fit_results = model.fit(x=inputs, y=outputs, epochs=100)
+model = k.Sequential(([
+        Dense(10, input_dim=inputs.shape[1]),
+        Activation('relu'),
+        Dense(5),
+        Activation('softmax')
+    ])
+)
+model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+fit_results = model.fit(x=inputs, y=outputs, epochs=10)
+
