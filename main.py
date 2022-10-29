b9500e45830b61f71100b412285174dc5b1306fc
Ivan <clicker.heroes.acg@gmail.com>
2022-10-29 18:56:25 +0300

    upd

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pandas as pd
 import keras as k
 import numpy as np
 from keras.layers import Dense, Activation
+from __future__ import absolute_import, division, print_function, unicode_literals
+import collections
+import tensorflow as tf
+from tensorflow.keras import layers
 
 df = pd.read_excel("dataset/dataset_train.xlsx")
 tmp = df.groupby(['Obfuscated name', 'Presentation']).agg({'Data': list, 'Class_label_FPG': list})
@@ -13,24 +17,17 @@ for i, items in accum["Data"].items():
     tmp = []
     for item in items:
         arrayFromStr = item.replace('[', '').replace(']', '').replace(' ', '').split(',')
-        arrayFromStr = [float(el) for el in arrayFromStr]
-        tmp.append(np.array(arrayFromStr))
+        tmp.append(np.array(arrayFromStr, dtype=float)[0:12])
     inputs.append(np.array(tmp))
 
 for i, items in accum["Class_label_FPG"].items():
-    temp = [float(elem) for elem in items]
-    outputs.append(np.array(temp))
+    outputs.append(np.array(items, dtype=float)[0:11])
 
-inputs = np.array(inputs)
-outputs = np.array(outputs)
+inputs = np.array(inputs, dtype=object)
+outputs = np.array(outputs, dtype=object)
 
-model = k.Sequential(
-    ([
-        Dense(40),
-        Activation('relu'),
-        Dense(20),
-        Activation('softmax')
-    ])
-)
-model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-fit_results = model.fit(x=inputs, y=outputs, epochs=10)
+model = tf.keras.Sequential()
+model.add(layers.Embedding(input_dim=1000, output_dim=64))
+model.add(layers.LSTM(128))
+model.add(layers.Dense(10))
+model.summary()
